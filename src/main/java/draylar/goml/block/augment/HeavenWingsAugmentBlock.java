ed4aef43a0bf0cd8a4cad6dcd98e9946e256b771
Nathan S. <nathan.smith.compsci@gmail.com>
2025-06-20 19:34:59 -0400

    Keep track of how many overlapping wings you have
    This ensures a premature removal of the wings doesn't happen.

--- src/main/java/draylar/goml/block/augment/HeavenWingsAugmentBlock.java
+++ src/main/java/draylar/goml/block/augment/HeavenWingsAugmentBlock.java
@@ -1,5 +1,9 @@
 package draylar.goml.block.augment;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.UUID;
+
 import draylar.goml.block.SelectiveClaimAugmentBlock;
 import io.github.ladysnake.pal.AbilitySource;
 import io.github.ladysnake.pal.Pal;
@@ -9,6 +13,7 @@ import net.minecraft.entity.player.PlayerEntity;
 public class HeavenWingsAugmentBlock extends SelectiveClaimAugmentBlock {
 
     public static final AbilitySource HEAVEN_WINGS = Pal.getAbilitySource("goml", "heaven_wings");
+    private static final Map<UUID, Integer> flightReferences = new HashMap<>();
 
     public HeavenWingsAugmentBlock(Settings settings, String texture) {
         super("heaven_wings", settings, texture);
@@ -16,11 +21,25 @@ public class HeavenWingsAugmentBlock extends SelectiveClaimAugmentBlock {
 
     @Override
     public void applyEffect(PlayerEntity player) {
-        HEAVEN_WINGS.grantTo(player, VanillaAbilities.ALLOW_FLYING);
+        UUID playerId = player.getUuid();
+        int currentRefs = flightReferences.getOrDefault(playerId, 0);
+        flightReferences.put(playerId, currentRefs + 1);
+
+        if (currentRefs == 0) {
+            HEAVEN_WINGS.grantTo(player, VanillaAbilities.ALLOW_FLYING);
+        }
     }
 
     @Override
     public void removeEffect(PlayerEntity player) {
-        HEAVEN_WINGS.revokeFrom(player, VanillaAbilities.ALLOW_FLYING);
+        UUID playerId = player.getUuid();
+        int currentRefs = flightReferences.getOrDefault(playerId, 0);
+
+        if (currentRefs > 1) {
+            flightReferences.put(playerId, currentRefs - 1);
+        } else {
+            flightReferences.remove(playerId);
+            HEAVEN_WINGS.revokeFrom(player, VanillaAbilities.ALLOW_FLYING);
+        }
     }
 }
